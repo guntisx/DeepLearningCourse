3fa9b08a99f5e7b981582e26120cfc7752bf786a
Guntis Barzdins <guntis@latnet.lv>
2024-05-08 16:33:12 +0300

    Update dog.py

--- other/dog/dog.py
+++ other/dog/dog.py
@@ -3,7 +3,7 @@ def dog(a):
     #while true:
 	for i in [1,2,3]:
 		if a == "mouse":
-			print("dog does not eat mouse, ignores it")
+			print("dog does not eat mice, ignores it")
 			continue
 		if a == "dog":
 			print("dog likes other dogs, snifs them")
